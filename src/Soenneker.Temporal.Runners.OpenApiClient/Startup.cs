9bc032e3be9b4b3e871c5dac55dec491816779ab
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-05-29 19:29:48 -0500

    openapi fixer

--- src/Soenneker.Temporal.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.Temporal.Runners.OpenApiClient/Startup.cs
@@ -5,6 +5,7 @@ using Soenneker.OpenApi.Fixer.Registrars;
 using Soenneker.Temporal.Runners.OpenApiClient.Utils;
 using Soenneker.Temporal.Runners.OpenApiClient.Utils.Abstract;
 using Soenneker.Utils.File.Download.Registrars;
+using Soenneker.Utils.Yaml.Registrars;
 
 namespace Soenneker.Temporal.Runners.OpenApiClient;
 
@@ -25,6 +26,7 @@ public static class Startup
                 .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
                 .AddRunnersManagerAsSingleton()
                 .AddOpenApiFixerAsSingleton()
+                .AddYamlUtilAsSingleton()
                 .AddFileDownloadUtilAsSingleton()
                 .AddKiotaUtilAsSingleton();
 
